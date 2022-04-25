94dfa7b292ec034e0c17c1336dc2c1624fba386a
Alexander Kosmehl <kosmehl@live.de>
2022-04-26 00:54:54 +0200

    added custom hook for puzzle generation logic

---
+++ src/Helper/useCrossword.ts
@@ -0,0 +1,10 @@
+import { CrosswordPuzzle } from './CrosswordPuzzle'
+
+export default function useCrossword(wordCount: number) {
+  const crossword: CrosswordPuzzle = null
+
+
+  return crossword
+}
+
+console.log(useCrossword(10))
