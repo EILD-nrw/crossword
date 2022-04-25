e91f1fbe9ab44fd200a3deb97cf42cb274a5018c
Alexander Kosmehl <kosmehl@live.de>
2022-04-26 00:58:50 +0200

    Added main container

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="max-w-screen-xl space-y-4 mx-auto my-0 flex flex-col justify-center">
       <Header />
+      <div className='bg-white'>
+        Content
+      </div>
       <Footer />
     </div>
   )
