6f1cb1c9e9d789779c73a450e9d0b54eb1b2e077
Alexander Kosmehl <kosmehl@live.de>
2022-04-25 14:52:00 +0200

    Added general components and tailwind

---
+++ src/Components/Header.tsx
@@ -0,0 +1,25 @@
+import edbHeaderLogo from '../img/edb_header_logo.png'
+import eildHeaderLogo from '../img/eild_header_logo.png'
+
+export const Header = () => {
+  return (
+    <header className="bg-white p-4">
+      <div className="w-full flex">
+        <div className="flex-auto">
+          <img
+            className="max-h-32 max-w-full"
+            src={edbHeaderLogo}
+            alt="EDB Logo"
+          />
+        </div>
+        <div className="flex-auto">
+          <img
+            className="max-h-32 max-w-full float-right"
+            src={eildHeaderLogo}
+            alt="EILD Logo"
+          />
+        </div>
+      </div>
+    </header>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'th-red': '#c81e0f',
+        'th-orange': '#ea5a00',
+        'th-violet': '#b43092'
+      }
+    },
+  },
+  plugins: [],
+}
