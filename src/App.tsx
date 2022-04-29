f6e6f9d54547cf9c73439e1002a7d53a0660efce
Alexander Kosmehl <kosmehl@live.de>
2022-04-30 01:33:26 +0200

    Renders simple crossword grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import { useState } from 'react'
+import { CrosswordCell } from './Components/CrosswordCell'
 import { Footer } from './Components/Footer'
 import { Header } from './Components/Header'
+import useCrossword from './Helper/useCrossword'
 
 function App() {
+  const { puzzle, refreshPuzzle } = useCrossword(10, 1)
+
   return (
     <div className="max-w-screen-xl space-y-4 mx-auto my-0 flex flex-col justify-center">
       <Header />
-      <div className='bg-white'>
-        Content
+      <div className="bg-white">
+        <table>
+          <tbody>
+            {puzzle?.cells.map((row, index) => {
+              return (
+                <tr key={index}>
+                  {row.map((cell, index) => (
+                    <CrosswordCell key={index} letter={cell} />
+                  ))}
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </div>
       <Footer />
     </div>
