60178bc728c31a65f89d10950329f589f935c535
Alexander Kosmehl <kosmehl@live.de>
2022-05-05 13:33:00 +0200

    Fixed rename

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Components/PuzzleContainer'
 import './index.css'
-import PuzzleContainer from './Components/PuzzleContainer'
+import { PuzzleContainer } from './Components/PuzzleContainer'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
